ba50c78d2a235f2edc7ed415af4462ab42eae7fa
Cloud-47 <ai7adlan@gmail.com>
2018-12-20 04:38:09 +0100

    Update main.cpp

--- RX/src/main.cpp
+++ RX/src/main.cpp
@@ -11,18 +11,28 @@ void setup(){
 }
 
 void loop(){
+   /*
+   *if the pin 5 ditect a signal do staff
+   */
    if(digitalRead(5)){
       bits1 = 0;
-      unsigned long deltaT = millis();
-      while(millis()-deltaT <= 10) if(digitalRead(5)) bits1 ++;
+      unsigned long deltaT = millis();//take the presint time storet in deltaT
+      /*
+      *in this loop we decode our bits
+      */
+      while(millis()-deltaT <= 10) if(digitalRead(5)) bits1 ++;//go to line 44
       //Serial.println(bits1);
+
       if(capture){
          boolean b = 0;
-         if(bits1 > 211 && bits1 < 400) b = 0;
-         if(bits1 > 70 && bits1 < 210) b = 1;
-         if(b) bitSet(CH,7-pos); else bitClear(CH,7-pos);
+         if(bits1 > 220 && bits1 < 400) b = 0;//here we decode our bits
+         if(bits1 > 100 && bits1 < 220) b = 1;
+         if(b) bitSet(CH,7-pos); else bitClear(CH,7-pos); // compose our carecter
          //Serial.println(b);
-         pos++;
+
+         pos++;//count the number of bits
+
+         //now print our carecter and reinisilise
          if(pos == 8){
             //Serial.println("\n");
             //Serial.println("ch is here ");
@@ -32,7 +42,7 @@ void loop(){
             capture = false;
          }
       }
-      if(bits1 > 600){
+      if(bits1 > 600){// if bits is more than 600 thas mean that it is signal inisilizition(TX code line 21)
          capture = true;
          pos = 0;
       }
